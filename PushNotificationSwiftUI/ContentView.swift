6d52c8a4f7ceb2c47fe68798f777b3d949a2f98a
chitomo12 <mrk3_12@yahoo.co.jp>
2021-12-04 21:36:30 +0900

    Initial Commit

---
+++ PushNotificationSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PushNotificationSwiftUI
+//
+//  Created by 福田正知 on 2021/12/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PushNotificationSwiftUI/PushNotificationSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PushNotificationSwiftUIApp.swift
+//  PushNotificationSwiftUI
+//
+//  Created by 福田正知 on 2021/12/04.
+//
+
+import SwiftUI
+
+@main
+struct PushNotificationSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
